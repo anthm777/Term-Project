1e4fbc02e4535230e71fe391cf5c37956e8bb0a1
Gagandeep Singh <97354588+anthm777@users.noreply.github.com>
2023-04-08 16:35:59 -0400

    Adding Admin Folder

---
+++ admin/src/components/table/Table.jsx
@@ -0,0 +1,83 @@
+import "./table.scss";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+
+const List = () => {
+  const rows = [
+    {
+      id: 854622,
+      product: "Gagandeep Singh",
+      img: "https://m.media-amazon.com/images/I/81bc8mA3nKL._AC_UY327_FMwebp_QL65_.jpg",
+      customer: "CSAM",
+      date: "Group 11",
+      amount: "Full Stack",
+    },
+    {
+      id: 856064,
+      product: "Parmjeet Kaur",
+      img: "https://m.media-amazon.com/images/I/31JaiPXYI8L._AC_UY327_FMwebp_QL65_.jpg",
+      customer: "CSAM",
+      date: "Group 11",
+      amount: "Front End, API"
+    },
+    {
+      id: 854330,
+      product: "Harpreet Kaur",
+      img: "https://m.media-amazon.com/images/I/71kr3WAj1FL._AC_UY327_FMwebp_QL65_.jpg",
+      customer: "CSAM",
+      date: "Group 11",
+      amount: "Front End",
+    },
+    {
+      id: 853051,
+      product: "Chetna",
+      img: "https://m.media-amazon.com/images/I/71wF7YDIQkL._AC_UY327_FMwebp_QL65_.jpg",
+      customer: "CSAM",
+      date: "Group 11",
+      amount: "Back End, API"
+      
+    },
+  ];
+  return (
+    <TableContainer component={Paper} className="table">
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell className="tableCell">Student ID</TableCell>
+            <TableCell className="tableCell">Student Name</TableCell>
+            <TableCell className="tableCell">Course</TableCell>
+            <TableCell className="tableCell">Group</TableCell>
+            <TableCell className="tableCell">Contribution</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow key={row.id}>
+              <TableCell className="tableCell">{row.id}</TableCell>
+              <TableCell className="tableCell">
+                <div className="cellWrapper">
+                  <img src={row.img} alt="" className="image" />
+                  {row.product}
+                </div>
+              </TableCell>
+              <TableCell className="tableCell">{row.customer}</TableCell>
+              <TableCell className="tableCell">{row.date}</TableCell>
+              <TableCell className="tableCell">{row.amount}</TableCell>
+              <TableCell className="tableCell">{row.method}</TableCell>
+              <TableCell className="tableCell">
+                <span className={`status ${row.status}`}>{row.status}</span>
+              </TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+};
+
+export default List;
